c5a4a6e2390b61b1e4b7325b57fdbc7a25b887a4
Valentina <valyuscha2004@gmail.com>
2019-03-27 11:33:31 +0200

    Made new programme

---
+++ script.js
@@ -0,0 +1,17 @@
+function calculator() {
+    var a = document.square.square_a.value
+    var b = document.square.square_b.value
+    var c = document.square.square_c.value
+    var sol = document.getElementById(' square_sol')
+
+    if (b == '-') {
+        string = a - c
+    } else if (b == '+') {
+        string = a + c
+    } else if (b == '*') {
+        string = a * c
+    } else if (b == '/') {
+        string = a / c
+    }
+    square_sol.innerHTML = string;
+}
